76253f491c389d2e269fd7b74739015c2d8d0bca
makerender <victordeluque@gmail.com>
2017-07-27 15:30:53 -0400

    js updates

---
+++ js/currency.js
@@ -0,0 +1,35 @@
+// var demo = function(data) {
+//   fx.rates = data.rates
+//   var rate = fx(1).from("GBP").to("USD")
+//   alert("£1 = $" + rate.toFixed(4))
+// }
+
+// $.getJSON("http://api.fixer.io/latest", demo)
+
+(() => {
+  'use strict'
+
+  // creating a new XMLHttpRequest.
+  const xhr = new XMLHttpRequest()
+  // HTTP method and where to request
+  xhr.open('GET', 'https://api.fixer.io/latest?symbols=USD,EUR,CAD')
+  // in a GET request what you send doesn't matter
+  // if POST this is the request body
+  xhr.send(null)
+
+  // wait for the 'readystatechange' event to fire on the xhr object
+  xhr.onreadystatechange = function () {
+    // if xhr has not finished we're not ready yet so return
+    if (xhr.readyState !== 4 ) { return }
+    // if it didn't get a 200 status back log the error
+    if (xhr.status !== 200) { return console.log('Error: ' + xhr.status) }
+
+    // if everything works log the response
+    console.log(xhr.responseText)
+
+    // logging api response
+    const logger = document.querySelector("#logger")
+
+    logger.innerHTML = xhr.responseText
+  }
+})()
